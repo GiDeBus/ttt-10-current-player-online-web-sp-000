834c36fd4e5b39ee2c54b77955a514493e2d16c5
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-21 22:18:13 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+#turn_count
+
+def turn_count(board)
+turn = 0
+  board.each do |counter|
+    if counter.to_s == 'X' or 'O'
+     turn += 1
+    else
+      turn = 0
+    end
+  end  
+end
+
+#current_player
+
+def current_player(board)
+  if turn_count(board).to_i % 2 == 0
+    puts 'X player turn'
+  else
+    puts 'O player turn'
+  end
+end
